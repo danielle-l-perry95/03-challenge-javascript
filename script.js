0636db6eeaf4426c8822ef7319edf4fe8ea15029
Danielle Perry <nsbd95@gmail.com>
2022-11-03 16:23:40 -0400

    moved around the starter code that was already written in JS

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+  var uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+  var specialCharacters = ['~','!','@','#','$','%','^','&','*',':',';','<','>',',','/','?','.'];
+
+
+var generateBtn = document.querySelector("#generate");
+// function generatePassword() {};
 function writePassword() {
-  var uppercaseValues = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var lowercaseValues = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var numericValues = ['0','1','2','3','4','5','6','7','8','9'];
-  var specialValues = ['~','!','@','#','$','%','^','&','*',':',';','<','>',',','/','?','.'];
-}
-
-function generatePassword() {};
-
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.value = password;}
 
 
 // Add event listener to generate button
