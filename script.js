ec3d366850a11d5e0dce4634831a642658efaaab
Danielle Perry <nsbd95@gmail.com>
2022-11-07 16:53:30 -0500

    changing my if else statements

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
 // Assignment Code
 
 // Write password to the #password input
-function createPassword () {
   var uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   var numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
   var specialCharacters = ['~','!','@','#','$','%','^','&','*',':',';','<','>',',','/','?','.'];
-}
 
-// combining the arrays for the anyCharacter.length part of the randomPassword function 
-let anyCharacter = uppercaseCharacters.concat(lowercaseCharacters,numericCharacters,specialCharacters);
+  var possibleCharacters = [];
+  var guaranteedCharacters = [];
+  var result = [];
 
+ 
   // write if, else if, else statement for number of characters btw 8-128
-  if (characterNumber > 8 && characterNumber < 128){
-    confirm ("Your password contains " + characterNumber + " characters.");
-  } else {
-    alert ("Your password must include between 8 and 128 characters.")
-  }
+  // if (characterNumber > 8 && characterNumber < 128){
+  //   confirm ("Your password contains " + characterNumber + " characters.");
+  // } else {
+  //   alert ("Your password must include between 8 and 128 characters.")
+  // }
 
-// Asking what character types to include in password
-var characterType = prompt ("What types of characters do you want to include in your password?");
-// write confirmation alerts to include uppercase, lowercase, numeric, and/or special characters if, else?
-var wantsUppercase = prompt ("Do you want to include uppercase characters in your password?");
-if (wantsUppercase){
-  var includeUppercase = alert ("You have selected to include uppercase characters.");
-}else{
-  alert ("You will not include any uppercase characters.");
+// two separate if statements one for less than 8 and one for greater than 128 true/false. Make sure you are given a number.
+var characterLength = prompt ("How many characters do you want to include?");
+if (characterLength < 8) {
+  alert ("Your password must include at leas 8 characters.")
+}else { confirm 
+} 
+
+if (characterLength > 128) {
+  alert ("Your password must be less than 128 characters.")
+}else {confirm
 }
 
-var wantsLowercase = prompt("Do you want to include lowercase characters in your password?");
-if (wantsLowercase){
-  var includeLowercase = alert ("You have selected to include lowercase characters.");
-}else{
-  alert ("You will not include any lowercase characters.");
+if (characterLength = Nan){
+  alert ("You must pick a numeric value.")
 }
+// write CONFIRMS to include uppercase, lowercase, numeric, and/or special characters. ALERT when user input is incorrect. Can be in one long if statement
+var wantsUppercase = window.confirm("Do you want to include uppercase characters in your password?")
+var wantsLowercase = window.confirm("Do you want to include lowercase characters in your password?")
+var wantsNumeric = window.confirm("Do you want to include numbers in your password?")
+var wantsSpecial = window.confirm("Do you want to include symbols in your password?")
 
-var wantsNumeric = prompt ("Do you want to include numbers in your password?");
-if (wantsNumeric){
-  var includeNumeric = alert ("You have selected to include numeric characters.");
-}else{
-  alert ("You will not include any numeric characters.");
-}
+// var wantsUppercase = prompt ("Do you want to include uppercase characters in your password?");
+// if (wantsUppercase){
+//   var includeUppercase = alert ("You have selected to include uppercase characters.");
+// }else{
+//   alert ("You will not include any uppercase characters.");
+// }
 
-var wantsSpecial = prompt("Do you want to include special characters in your password?");
-if (wantsSpecial){
-  var includeSpecial = alert ("You have selected to include special characters.");
-}else{
-  alert ("You will not include any special characters.");
-}
+// var wantsLowercase = prompt("Do you want to include lowercase characters in your password?");
+// if (wantsLowercase){
+//   var includeLowercase = alert ("You have selected to include lowercase characters.");
+// }else{
+//   alert ("You will not include any lowercase characters.");
+// }
 
-// Validate input and at least one character type should be selected 
+// var wantsNumeric = prompt ("Do you want to include numbers in your password?");
+// if (wantsNumeric){
+//   var includeNumeric = alert ("You have selected to include numeric characters.");
+// }else{
+//   alert ("You will not include any numeric characters.");
+// }
+
+// var wantsSpecial = prompt("Do you want to include special characters in your password?");
+// if (wantsSpecial){
+//   var includeSpecial = alert ("You have selected to include special characters.");
+// }else{
+//   alert ("You will not include any special characters.");
+// }
+
  
 
 // function generatePassword() {};
@@ -60,14 +77,14 @@ function writePassword() {
 }
 
 
-// write random password generator. Do I need to include i=8, i <= 128?
+// write random password generator. if/else statement for uppercase array, lowercase array etc 
   function randomPassword () {
-    for (var i= 8; i <= 128; i++){
-    var randomNumber = Math.floor(Math.random()* anyCharacter.length); /*should it be anyCharacter.length? */
+    for (var i= 0; i <= character.length; i++){
+    var randomNumber = Math.floor(Math.random()); 
     return randomPassword;
   }
   
- // write alert once password is generated 
+ // write alert once password is generated. If generatePassword = true then alert?
 function passwordGenerated(){
   alert ('Your password has been created');
 }
